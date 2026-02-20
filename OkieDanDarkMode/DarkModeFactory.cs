569b44f468c685d10756ff99eb39975a4d02f1e6
Djinnet <the.gaming.djinn@gmail.com>
2026-02-20 22:57:09 +0100

    Built-in dark mode, custom controls, dialogs, CI added
    Replaced NuGet dark mode with built-in implementation, added custom flat controls (ComboBox, ProgressBar, TabControl), and introduced a comprehensive Messenger utility for custom dialogs with dark mode and localization. Updated project versioning, removed package dependency, and added GitHub Actions workflow for automated build and release. Project is now self-contained, modern, and supports flexible theming and custom dialogs.

---
+++ OkieDanDarkMode/DarkModeFactory.cs
@@ -0,0 +1,13 @@
+﻿using OkieDan.Forms.DarkModeCore;
+using System.Windows.Forms;
+
+public class DarkModeFactory : IDarkModeFactory
+{
+    public DarkModeCS Create(Form form)
+    {
+        return new DarkModeCS(form)
+        {
+            ColorMode = DarkModeCS.DisplayMode.SystemDefault
+        };
+    }
+}
---
+++ OkieDanDarkMode/IDarkModeFactory.cs
@@ -0,0 +1,6 @@
+﻿using OkieDan.Forms.DarkModeCore;
+using System.Windows.Forms;
+public interface IDarkModeFactory
+{
+    DarkModeCS Create(Form form);
+}
